0792def89f611c7a2b40bb2bcd033f35f9770681
ZIDD <156231466+AzidMuhammad@users.noreply.github.com>
2024-04-06 02:48:27 +0700

    Update Project.vue

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -88,15 +88,15 @@ export default {
           name: 'All',
           images: [
             {
-              src: '../src/assets/image/figma.png',
+              src: '/assets/image/figma.png',
               caption: 'go-kel app design figma project'
             },
             {
-              src: '../src/assets/image/poke.png',
+              src: '/assets/image/poke.png',
               caption: 'pokelapak top up website'
             },
             {
-              src: '../src/assets/image/fog.jpg',
+              src: '/assets/image/fog.jpg',
               caption: 'freedom of games (games download website)'
             }
           ]
@@ -105,11 +105,11 @@ export default {
           name: 'Web Development',
           images: [
             {
-              src: '../src/assets/image/poke.png',
+              src: '/assets/image/poke.png',
               caption: 'pokelapak top up website'
             },
             {
-              src: '../src/assets/image/fog.jpg',
+              src: '/assets/image/fog.jpg',
               caption: 'freedom of games (games download website)'
             }
           ],
@@ -119,7 +119,7 @@ export default {
           name: 'UI And UX',
           images: [
             {
-              src: '../src/assets/image/figma.png',
+              src: '/assets/image/figma.png',
               caption: 'go-kel app design figma project'
             }
           ]
